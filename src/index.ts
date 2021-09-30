0973198ec0c36bcbc56d69b396ddf4ba52ebd51e
Majoneza <matejbozic1@hotmail.com>
2021-10-01 00:17:58 +0200

    commitment6

--- src/index.ts
+++ src/index.ts
@@ -7,39 +7,30 @@ function getTemplate(pingerWebsite: string, port: number, pingerInterval: number
     return `const express = require('express');
 const process = require('process');
 const PORT = process.env.PORT || ${port};
+const https = require('https');
 const { Worker } = require('worker_threads');
 
-const worker1 = new Worker('${main_script_path.startsWith('./') ? main_script_path : ('./' + main_script_path)}');
-worker1.on('message', (m) => console.log('Worker message: ' + m));
-worker1.on('error', (e) => console.log('Worker error: ' + e));
-worker1.on('exit', (code) => {
+const worker = new Worker('${main_script_path.startsWith('./') ? main_script_path : ('./' + main_script_path)}');
+worker.on('message', (m) => console.log('Worker message: ' + m));
+worker.on('error', (e) => console.log('Worker error: ' + e));
+worker.on('exit', (code) => {
 	console.log('Worker exited with code: ' + code);
 });
-
-const worker2 = new Worker(new URL(\`data:text/javascript,
-const { parentPort } = require('worker_threads')
-const https = require('https')
 
 setInterval(() => {
 	https.get('${pingerWebsite}', (res) => {
 		res.on('data', (chunk) => {
 			try {
-				parentPort.postMessage('WEBSITE RESPONSE: ' + chunk);
+				console.log('WEBSITE RESPONSE: ' + chunk);
 			}
 			catch(err) {
-				parentPort.postMessage('Pinger http chunk crashed with ' + err.message);
+				console.error('Pinger http chunk crashed with ' + err.message);
 			}
 		});
 	}).on('error', (err) => {
-		parentPort.postMessage('Pinger http crashed with: ' + err.message);
+		console.error('Pinger http crashed with: ' + err.message);
 	});
 }, ${pingerInterval} * 1000);
-\`));
-worker2.on('message', (m) => console.log('Worker message: ' + m));
-worker2.on('error', (e) => console.log('Worker error: ' + e));
-worker2.on('exit', (code) => {
-	console.log('Worker exited with code: ' + code);
-});
 
 express()
     .get('/', (req, res) => res.sendStatus(200))
